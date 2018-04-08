0f123cb583d02fadf77fa95582d9b9c20edf68a1
Will Hodge <will.hodge18@gmail.com>
2018-04-08 12:00:47 +0100

    updated authentication

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 let access_token;
+let songs_retrieved = false;
+let artists_retrieved = false;
 
 function initialize() {
 
   /* adds an event listener to the button and takes page to created URL */
   document.getElementById('login-button').addEventListener('click', function() {
     let client_id = '698842fbb3c04667be310ea4326af018';
-    let redirect_uri = 'https://will-hodge.github.io/top-tunes/';
-
+    let redirect_uri = 'http://127.0.0.1:3000';
+    let scopes = 'user-top-read';
     /* creates authorization URL */
     let url = 'https://accounts.spotify.com/authorize';
     url += '?response_type=token';
     url += '&client_id=' + encodeURIComponent(client_id);
+    url += '&scope=' + encodeURIComponent(scopes);
     url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
 
     window.location = url;
@@ -39,14 +42,17 @@ function getTopArtists(){
     $.ajax({
       url: 'https://api.spotify.com/v1/me/top/artists',
       headers: {
-        'Authorization': 'Bearer ' + access_token
+        'Authorization': 'Bearer ' + access_token,
       },
       success: function(response) {
-        if (response.items.length != 0){
-          for (artist in response.items) {
-            $('#results').append('<div class="artist"><img src=' + artist.images[0].url + '>' + '<h3 class="ui header centered>"' + artist.name + '</h3></div>');
-          }
+        if (artists_retrieved) {
+          return;
         }
+        for (let i = 0; i < response.items.length; i++) {
+          console.log(response.items[i]);
+          $('#results').append('<img src=' + response.items[i].images[0].url + '>');
+        }
+        artists_retrieved = true;
       },
       error: function(jqXHR, textStatus, errorThrown) {
         alert('Unable to authorize through Spotify Web API (Error ' + jqXHR.status + ')');
@@ -95,10 +101,6 @@ $(document).ready(function() {
   initialize();
   access_token = getHashValue('access_token');
   if (access_token){
-    $('#header').html("SpotAffect");
-    $('.running').show();
-    $('.login').hide();
-    $('#active').addClass("disabled");
   }
 });
 
